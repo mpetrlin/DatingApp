5d4883bd3da9a547bab20f77bdd926792ac7db1b
Matej Petrlin <matej.petrlin@outlook.com>
2021-06-14 10:50:40 +0200

    End of section 16

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -31,6 +31,7 @@ namespace API.Controllers
             _userRepository = userRepository;
         }
 
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
